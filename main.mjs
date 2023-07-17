828f5b9e2b186937422267bd7708a063ec5e40e2
Kamala <kamalashamilova@gmail.com>
2023-07-18 00:05:07 +0400

    10-17 course

---
+++ main.mjs
@@ -0,0 +1,6 @@
+import math from "./math-esm.mjs";
+
+const { add, subract } = math;
+
+console.log(math.add(9, 29));
+console.log(math.subtract(9, 29));
---
+++ math-esm.mjs
@@ -0,0 +1,20 @@
+//ES modules
+
+const add = (a, b) => {
+  return a + b;
+};
+
+// export default add
+
+// export default (a,b)=>{
+//     return a+b;
+// }
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+export default {
+  add,
+  subtract,
+};
